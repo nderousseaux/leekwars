98070ba5ed218654222b37990da51824ad15cdf9
n.derousseaux <n.derousseaux@unistra.fr>
2025-04-08 14:12:59 +0200

    feat: improving push script

--- push.py
+++ push.py
@@ -1,47 +1,55 @@
 """
 Force-push code to LeekWars by **erasing every AI file and folder** on the website
 and copying everything from the local selected folder to the server
-
-Instructions: Setup the session cookie credentials in creds.py, and change the local folder \
-	to push to the remote (LOCAL_FOLDER="ai-code" by default)
 """
 
 import os
 import requests
 
-import creds
+# Get .env variables
+from dotenv import load_dotenv
+load_dotenv()
+URL = os.getenv("URL")
+LOCAL_FOLDER = os.getenv("LOCAL_FOLDER")
+USERNAME = os.getenv("USERNAME")
+PASSWORD = os.getenv("PASSWORD")
 
+def irequest(token, method: str, url: str, **kwargs) -> requests.Response:
+	"""
+	Wrapper for requests to handle errors
+	"""
 
-URL = "https://leekwars.com/api"
-SESSION_COOKIE = {
-	"token": creds.TOKEN,
-	"lang": "fr",
-	"wt": "decoded null",
-	"PHPSESSID": creds.PHPSESSID
-}
+	cookies = {
+		"token": token
+	}
 
-LOCAL_FOLDER = "src"
+	try:
+		response = requests.request(method, url, cookies=cookies, **kwargs)
+		response.raise_for_status()
+		return response
+	except requests.RequestException as e:
+		print(f"Request failed: {e}")
+		return None
 
-
-def delete_all_remote() -> None:
+def delete_all_remote(token) -> None:
 	"""
 	Delete all files and folders on server
 	"""
-	farmerAIs = requests.get(f"{URL}/ai/get-farmer-ais", cookies=SESSION_COOKIE).json()
+	farmerAIs = irequest(token, "GET", f"{URL}/ai/get-farmer-ais").json()
 	ais = farmerAIs["ais"]
 	folders = farmerAIs["folders"]
 
 	for ai in ais:
-		requests.delete(f"{URL}/ai/delete/", cookies=SESSION_COOKIE, data={
+		irequest(token, "DELETE", f"{URL}/ai/delete/", data={
 			"ai_id": ai["id"]
 		})
 
 	for folder in folders:
-		requests.delete(f"{URL}/ai-folder/delete/", cookies=SESSION_COOKIE, data={
+		irequest(token, "DELETE", f"{URL}/ai-folder/delete/", data={
 			"folder_id": folder["id"]
 		})
 
-def fill_subtree(subtree_root: str, subtree_root_id: int) -> None:
+def fill_subtree(token, subtree_root: str, subtree_root_id: int) -> None:
 	"""
 	Fill the remote subtree to match its local counterpart
 	Copy files and recursively fill subfolders
@@ -56,45 +64,53 @@ def fill_subtree(subtree_root: str, subtree_root_id: int) -> None:
 		file_full_path = os.path.join(subtree_root, filename)
 
 		if os.path.isdir(file_full_path):
-			folder_id = create_remote_folder(filename, subtree_root_id)
-			fill_subtree(file_full_path, folder_id)
+			folder_id = create_remote_folder(token, filename, subtree_root_id)
+			fill_subtree(token, file_full_path, folder_id)
 		else:
 			with open(file_full_path, "r") as f:
 				content = f.read()
-				create_remote_file(filename, content, subtree_root_id)
+				create_remote_file(token, filename, content, subtree_root_id)
 
-def create_remote_folder(name: str, parent_id: int) -> int:
+def create_remote_folder(token, name: str, parent_id: int) -> int:
 	"""
 	Create a folder on server
 	"""
-	response = requests.post(f"{URL}/ai-folder/new-name/", cookies=SESSION_COOKIE, data={
+	response = irequest(session, "POST", f"{URL}/ai-folder/new-name/", data={
 		"name": name,
 		"folder_id": parent_id
 	})
 	return response.json()["id"]
 
-def create_remote_file(name: str, content: str, folder_id: int) -> None:
+def create_remote_file(session, name: str, content: str, folder_id: int) -> None:
 	"""
 	Create a file on server
 	"""
-	response = requests.post(f"{URL}/ai/new-name/", cookies=SESSION_COOKIE, data={
+	response = irequest(session, "POST", f"{URL}/ai/new-name/", data={
 		"name": name,
 		"version": 4,
 		"folder_id": folder_id
 	})
 	ai_id = response.json()["ai"]["id"]
 
-	response = requests.post(f"{URL}/ai/save/", cookies=SESSION_COOKIE, data={
+	irequest(session, "POST", f"{URL}/ai/save/", data={
 		"ai_id": ai_id,
 		"code": content
 	})
 
-	response = requests.put(f"{URL}/ai/strict/", cookies=SESSION_COOKIE, data={
+	irequest(session, "PUT", f"{URL}/ai/strict", data={
 		"ai_id": ai_id,
-		"strict": True
+		"strict": "true"
 	})
 
 
 if __name__ == "__main__":
-	delete_all_remote()
-	fill_subtree(LOCAL_FOLDER, 0)
+	with requests.Session() as session:
+		# Login to LeekWars
+		res = irequest("", "POST", f"{URL}/farmer/login-token", data={
+			"login": USERNAME,
+			"password": PASSWORD
+		})
+		token = res.json()["token"]
+
+		delete_all_remote(token)
+		fill_subtree(token, LOCAL_FOLDER, 0)
