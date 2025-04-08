6b866501fb6e5b680b7a8ee4516a75f459351571
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2025-04-08 13:41:29 +0200

    ✨ feat: adding script for pushing

--- push.py
+++ push.py
@@ -1,46 +1,100 @@
+"""
+Force-push code to LeekWars by **erasing every AI file and folder** on the website
+and copying everything from the local selected folder to the server
+
+Instructions: Setup the session cookie credentials in creds.py, and change the local folder \
+	to push to the remote (LOCAL_FOLDER="ai-code" by default)
+"""
+
 import os
 import requests
 
-name = "LeekIA"
-folder = "leek-ia"
+import creds
 
-url = "https://leekwars.com/api"
-c = {
-	"token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3NTI1ODY4MTMsImlkIjoxMjExNzUsImtlZXAiOnRydWUsImhhc2giOiIkMnkkMTAkU2dsTzF0TmV6cnhiYyIsImlwIjoiMTA5LjE0LjIzOC40IiwiaXNzIjoiaHR0cHM6XC9cL2xlZWt3YXJzLmNvbSIsImF1ZCI6Imh0dHBzOlwvXC9sZWVrd2Fycy5jb20ifQ.uwogTIybhONGfyPHqd2eAzEJLxkXO5Aglf2DwWAVOKk",
+
+URL = "https://leekwars.com/api"
+SESSION_COOKIE = {
+	"token": creds.TOKEN,
 	"lang": "fr",
 	"wt": "decoded null",
-	"PHPSESSID": "gtb0a6odjv5m7c812huujegqav"
+	"PHPSESSID": creds.PHPSESSID
 }
 
-def create_file(name, content, folder=0):
-	response = requests.post(f"{url}/ai/new-name/", cookies=c, data={
+LOCAL_FOLDER = "src"
+
+
+def delete_all_remote() -> None:
+	"""
+	Delete all files and folders on server
+	"""
+	farmerAIs = requests.get(f"{URL}/ai/get-farmer-ais", cookies=SESSION_COOKIE).json()
+	ais = farmerAIs["ais"]
+	folders = farmerAIs["folders"]
+
+	for ai in ais:
+		requests.delete(f"{URL}/ai/delete/", cookies=SESSION_COOKIE, data={
+			"ai_id": ai["id"]
+		})
+
+	for folder in folders:
+		requests.delete(f"{URL}/ai-folder/delete/", cookies=SESSION_COOKIE, data={
+			"folder_id": folder["id"]
+		})
+
+def fill_subtree(subtree_root: str, subtree_root_id: int) -> None:
+	"""
+	Fill the remote subtree to match its local counterpart
+	Copy files and recursively fill subfolders
+	"""
+	subtree_root.split("/")[-1]
+
+	for filename in os.listdir(subtree_root):
+		# Files and directories starting with _ are considered to be hidden and are not pushed
+		if filename.startswith("_"):
+			continue
+
+		file_full_path = os.path.join(subtree_root, filename)
+
+		if os.path.isdir(file_full_path):
+			folder_id = create_remote_folder(filename, subtree_root_id)
+			fill_subtree(file_full_path, folder_id)
+		else:
+			with open(file_full_path, "r") as f:
+				content = f.read()
+				create_remote_file(filename, content, subtree_root_id)
+
+def create_remote_folder(name: str, parent_id: int) -> int:
+	"""
+	Create a folder on server
+	"""
+	response = requests.post(f"{URL}/ai-folder/new-name/", cookies=SESSION_COOKIE, data={
 		"name": name,
-		"version": 1,
-		"folder_id": folder if folder else 0,
+		"folder_id": parent_id
+	})
+	return response.json()["id"]
+
+def create_remote_file(name: str, content: str, folder_id: int) -> None:
+	"""
+	Create a file on server
+	"""
+	response = requests.post(f"{URL}/ai/new-name/", cookies=SESSION_COOKIE, data={
+		"name": name,
+		"version": 4,
+		"folder_id": folder_id
+	})
+	ai_id = response.json()["ai"]["id"]
+
+	response = requests.post(f"{URL}/ai/save/", cookies=SESSION_COOKIE, data={
+		"ai_id": ai_id,
+		"code": content
 	})
 
-	response = requests.post(f"{url}/ai/save/", cookies=c, data={
-		"ai_id": response.json()["ai"]["id"],
-		"code": content,
-	})		
-
-
-# Get ias 
-response = requests.get(f"{url}/ai/get-farmer-ais", cookies=c)
-ais = response.json()["ais"]
-
-# Delete all ia on server
-for i in ais:
-	response = requests.delete(f"{url}/ai/delete/", cookies=c, data={
-		"ai_id": i["id"],
+	response = requests.put(f"{URL}/ai/strict/", cookies=SESSION_COOKIE, data={
+		"ai_id": ai_id,
+		"strict": True
 	})
 
-# For all file in the folder ./folder
-for file in os.listdir(folder):
-	# Create the file on the server
-	with open(os.path.join(folder, file), "r") as f:
-		content = f.read()
-		name = file.split(".")[0]
-		# Create the file on the server
-		create_file(name, content, folder=0)
 
+if __name__ == "__main__":
+	delete_all_remote()
+	fill_subtree(LOCAL_FOLDER, 0)
